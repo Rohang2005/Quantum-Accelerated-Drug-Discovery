9d13cbb5608090a6fb239be0189a5eac8ffe77b5
Rohang2005 <157229021+Rohang2005@users.noreply.github.com>
2026-03-01 18:18:17 +0530

    Delete check.py

--- check.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAI-tCA4WZwfHNpnrAa2F_CeaGaHctYK1E")
-
-for model in genai.list_models():
-    if "generateContent" in model.supported_generation_methods:
-        print(model.name)
